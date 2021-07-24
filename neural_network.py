1e0c2cce80a43f85c2857e81b90ddbeeb68c073b
Ava <ava.oveisi.78@gmail.com>
2021-07-24 17:04:55 -0400

    removed asinh loss, switched to r,phi for integrated set, organized files and datasets

--- neural_network.py
+++ neural_network.py
@@ -4,15 +4,10 @@ from loading_data import *
 import random
 import time
 
-#set of activation functions
-reluAct = ['ReLU', 'Linear'] # using this! works the best
-tanhAct = ['tanh', 'Linear']
-softplusAct = ['Softplus', 'Linear']
-#constant vars
-LR = 0.001
+# set up
 BATCH_SIZE = 10
-NUM_EPOCHS = 100
-NUM_HIDDNEURONS = 256
+EPOCHS = 100
+HIDDEN_NEURONS = 256
 STEPS_PER_EPOCH = NUM_TRAIN//BATCH_SIZE
 lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
   0.01, #initial learning rate
@@ -21,11 +16,12 @@ lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
   staircase=False)
 optimizer = tf.keras.optimizers.Adam(lr_schedule)
 
-# >FIXME don't know if this works yet
 def plot_NN_loss(train_loss, val_loss, trainLossLabel='loss', valLossLabel='val_loss', title = 'Training vs Validation Loss'):
-    '''Plot val and train loss over epochs'''
-    plt.plot(train_loss, label=trainLossLabel, color='blue', linestyle='-', linewidth = 1, marker = 'o', ms = 1, markeredgecolor='black', markeredgewidth=0.2)
-    plt.plot(val_loss, label=valLossLabel,color='red', linestyle='dashed', linewidth = 1, ms = 1, markeredgecolor='black', markeredgewidth=0.2)
+    '''Plot val loss and train loss over epochs'''
+    plt.plot(train_loss, label=trainLossLabel, color='blue', linestyle='-', linewidth = 1, 
+             marker = 'o', ms = 2, markeredgecolor='black', markeredgewidth=0.2)
+    plt.plot(val_loss, label=valLossLabel, color='red', linestyle='dashed', linewidth = 1, 
+             marker = 'o', ms = 2, markeredgecolor='black', markeredgewidth=0.2)
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.legend()
@@ -33,19 +29,18 @@ def plot_NN_loss(train_loss, val_loss, trainLossLabel='loss', valLossLabel='val_
     plt.title(title)
 
 @tf.function
-def loss_fn(y_true, y_pred, train=False, lin_loss=True, error=False):
-    '''Return loss for data with error bars in both X and Y if error=True or loss for data with no errors if error=False
-    note: y_true has shape (BATCH_SIZE) its a batch either from Yo_train or Yo_valid
-    note: y_pred has shape (BATCH_SIZE * 10), prediction of model when given a batch of X that has 10 samples per item
-    set train to true or false based on which set the loss function is given, training or validation
-    set lin_loss to true or false based on if loss function is linear or asinh'''
+def loss_fn(y_true, y_pred, train=False, error=False):
+    '''Return loss for data with error bars in both X and Y if error=True else return loss for data with no errors
+    y_true has shape (BATCH_SIZE); its a batch either from Yo_train or Yo_valid
+    y_pred has shape (BATCH_SIZE * 10); its prediction of model when given a batch of X that has 10 samples per item
+    Set train to true if given a training set, else if given a set validation set, set train to false'''
     # to resolve type mismatch
     y_true = tf.cast(y_true, tf.double)
     y_pred = tf.cast(y_pred, tf.double)
     loss = 0
     if not error: # no errors in either X or Y
         for i in range(BATCH_SIZE):
-            loss += tf.square(y_true[i] - y_pred[i]) if lin_loss else tf.square(tf.math.asinh(y_pred[i]) - tf.math.asinh(y_true[i]))
+            loss += tf.square(y_true[i] - y_pred[i]) 
         tf.cast(loss, tf.float64)
         return loss / BATCH_SIZE
     # errors in both X and Y
@@ -57,13 +52,13 @@ def loss_fn(y_true, y_pred, train=False, lin_loss=True, error=False):
     for i in range(BATCH_SIZE):
         for j in range(10):
             # calculate lin or asinh difference
-            diff = tf.math.subtract(y_true[i], y_pred[i][j]) if lin_loss else tf.math.subtract(tf.math.asinh(y_true[i]), tf.math.asinh(y_pred[i][j]))
+            diff = tf.math.subtract(y_true[i], y_pred[i][j]) 
             diff = tf.cast(diff, tf.float64)
             loss += tf.math.divide(tf.square(diff), np.square(Ye[i]))
     return loss / (BATCH_SIZE * 10)
 
 @tf.function
-def train_step(x_batch_train, y_batch_train, model, lin_loss=True, error=False):
+def train_step(x_batch_train, y_batch_train, model, error=False):
     '''Return train loss for a training X and Y batch'''
     # open a GradientTape to record the operations run during the forward pass, which enables auto-differentiation
     with tf.GradientTape() as tape:
@@ -72,18 +67,18 @@ def train_step(x_batch_train, y_batch_train, model, lin_loss=True, error=False):
             x_batch_train = tf.reshape(x_batch_train, [BATCH_SIZE * 10, 2]) 
         # give loss_fn BATCHSIZE outputs from model if error=False else give loss_fn (BATCHSIZE * 10) outputs from model() then find loss with BATCHSIZE Y
         logits = model(x_batch_train, training=True)  # logits for this minibatch
-        loss_value = loss_fn(y_batch_train, logits, train=True, lin_loss=lin_loss, error=error) # loss value for this minibatch  
+        loss_value = loss_fn(y_batch_train, logits, train=True, error=error) # loss value for this minibatch  
     grads = tape.gradient(loss_value, model.trainable_weights)
     optimizer.apply_gradients(zip(grads, model.trainable_weights))
     return loss_value
 
 @tf.function
-def val_step(x_batch_valid, y_batch_valid, model, lin_loss=True, error=False):
+def val_step(x_batch_valid, y_batch_valid, model, error=False):
     '''Return loss loss for a validation X and Y batch'''
     if error:
         x_batch_valid = tf.reshape(x_batch_valid, [BATCH_SIZE * 10, 2]) 
     val_logits = model(x_batch_valid, training=False)
-    loss_value = loss_fn(y_batch_valid, val_logits, train=False, lin_loss=lin_loss, error=error) 
+    loss_value = loss_fn(y_batch_valid, val_logits, train=False, error=error) 
     return loss_value
 
 def get_NN_pred(model, X_data, error=False):
@@ -107,9 +102,10 @@ def get_NN_model(monotonic, error=False):
     inputs = keras.Input(shape=[2,])
     x = normalizer(inputs)
     # >FIXME make sure all weights are positive after this? use constraints class in tf
-    x = layers.Dense(NUM_HIDDNEURONS, activation="relu", name="dense_1")(x) if not monotonic else layers.Dense(NUM_HIDDNEURONS, activation="relu", name="dense_1", kernel_initializer=my_init(2., 1.))(x)
-    x = layers.Dense(NUM_HIDDNEURONS, activation="relu", name="dense_2")(x) if not monotonic else layers.Dense(NUM_HIDDNEURONS, activation="relu", name="dense_2", kernel_initializer=my_init(2., 1.))(x)
-    x = layers.Dense(NUM_HIDDNEURONS, activation="relu", name="dense_3")(x) if not monotonic else layers.Dense(NUM_HIDDNEURONS, activation="relu", name="dense_3", kernel_initializer=my_init(2., 1.))(x)
+    x = layers.Dense(HIDDEN_NEURONS, activation="relu", name="dense_1")(x) if not monotonic else layers.Dense(HIDDEN_NEURONS, activation="relu", name="dense_1", kernel_initializer=my_init(2., 1.))(x)
+    x = layers.Dense(HIDDEN_NEURONS, activation="relu", name="dense_2")(x) if not monotonic else layers.Dense(HIDDEN_NEURONS, activation="relu", name="dense_2", kernel_initializer=my_init(2., 1.))(x)
+    x = layers.Dense(HIDDEN_NEURONS, activation="relu", name="dense_3")(x) if not monotonic else layers.Dense(HIDDEN_NEURONS, activation="relu", name="dense_3", kernel_initializer=my_init(2., 1.))(x)
+    # activation is linear if not specified
     outputs = layers.Dense(1, name="predictions")(x) if not monotonic else layers.Dense(1, name="predictions", kernel_initializer=my_init(2., 1.))(x)
     model = keras.Model(inputs=inputs, outputs=outputs)
     return model
@@ -131,10 +127,9 @@ class my_init(tf.keras.initializers.Initializer):
     def get_config(self):  # >FIXME ( ASK ??? need this?)To support serialization
       return {'mean': self.mean, 'stddev': self.stddev}
 
-def train_and_test_NN_model(lin_loss=True, monotonic=True, error=False):
+def train_and_test_NN_model(monotonic=True, error=False):
     '''Return validation and training loss data over epochs 
-    Source: https://www.tensorflow.org/guide/keras/writing_a_training_loop_from_scratch
-    set lin_loss to true or false based on if loss function is linear or asinh'''
+    Source: https://www.tensorflow.org/guide/keras/writing_a_training_loop_from_scratch'''
     # prepare training and validation sets
     # shuffle validation and training indecies to randomize batching for Xo and Yo
     train_ind, valid_ind = [i for i in range(NUM_TRAIN)], [i for i in range(NUM_VALID)] # to get same shuffle indecies for X and Y
@@ -153,13 +148,13 @@ def train_and_test_NN_model(lin_loss=True, monotonic=True, error=False):
 
     # list of val loss and train loss data for plotting
     val_loss, train_loss = [], []
-    for epoch in range(NUM_EPOCHS):
+    for epoch in range(EPOCHS):
         start_time = time.time()
         print("\nStart of epo ch %d" % (epoch,))
         
         # iterate over batches - note: x_batch_train has shape (BATCH_SIZE * 10 * 2) and y_batch_train has shape (BATCH_SIZE)
         for step, (x_batch_train, y_batch_train) in enumerate(zip(X_train_batched, Y_train_batched)):
-            loss_value = train_step(x_batch_train, y_batch_train, model, lin_loss, error=error)
+            loss_value = train_step(x_batch_train, y_batch_train, model, error=error)
             # appending was here
             # log every 10 batches - note: for training we have 60 batches and for validation we have 20 batches
             # print("STEP: ", step, len(X_train_batched), len(Y_train_batched))
@@ -168,7 +163,7 @@ def train_and_test_NN_model(lin_loss=True, monotonic=True, error=False):
                 print("Seen so far: %s samples" % ((step + 1) * BATCH_SIZE))
         # run validation loop at the end of each epoch.
         for (x_batch_valid, y_batch_valid) in zip(X_valid_batched, Y_valid_batched):
-            val_loss_value = val_step(x_batch_valid, y_batch_valid, model, lin_loss, error=error)
+            val_loss_value = val_step(x_batch_valid, y_batch_valid, model, error=error)
             # appending was here
         train_loss.append(loss_value) # save train loss for plotting
         val_loss.append(val_loss_value) # save val loss for plotting
